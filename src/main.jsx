6d018ffd72aedaf7e52558a58ea6d6aa03506d63
MarinaV38 <marisabell21@bk.ru>
2025-11-20 19:03:44 +0300

    Подготовка окружения. Щаг 1

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '@hexlet/testing-task-manager'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {App()}
+  </React.StrictMode>,
+)
